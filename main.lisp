93bd6d4a7b872ddad0acda4aacc2ac37bd41a09a
root <root@JellyFish.myguest.virtualbox.org>
2023-08-17 22:54:21 +0800

    更新了关于迷宫结构存储部分的Common Lisp源码，但并未实现完全；更新了_ReadMe.md中关于迷宫结构存储部分的说明

---
+++ main.lisp
@@ -0,0 +1,48 @@
+
+;;;; 存储迷宫结构的数据结构
+
+; 1. 双层列表嵌套
+; 2. 外层列表作为迷宫的行，内层列表作为迷宫的列
+; 3. 内层列表内的列表作为迷宫的节点
+; 4. 迷宫节点的数据结构定义为
+;    (prev (dir1 dir2 dir2))
+;    1) 其中prev表示上一个节点，即由何方位的相邻节点到达该节点
+;    2) 如果是迷宫生成的起始点，则prev 为'nil
+;    3) dir1, dir2, dir3表示下一节点，即向何方位到达相通的相邻节点
+;    4) 方位均由'w, 'a, 's, 'd表示
+
+(defparameter *W-maze* 20)
+(defparameter *H-maze* 20)
+
+(defun generate-table ()
+  (let ((table nil))
+    (dotimes (i *H-maze*)
+      (let ((single-row nil))
+        (dotimes (j *W-maze*)
+          (setq single-row (cons '(nil ()) single-row)))
+        (setq table (cons single-row table))))
+    table))
+
+(defparameter *table* (generate-table))
+
+(defun get-node (i j)
+  "根据下标访问元素。根据(i j)坐标访问第i行、第j列节点"
+  (nth j (nth i *table*)))
+
+(defun neibour-node (i j dir step)
+  "从节点(i j)出发，向dir方向行走step布"
+  (let ((i-next i)
+        (j-next j))
+    (cond ((eql dir 'w) (setq i-next (- i step)))
+          ((eql dir 'a) (setq j-next (- j step)))
+          ((eql dir 's) (setq i-next (+ i step)))
+          ((eql dir 'd) (setq j-next (+ j step))))
+    `(,i-next ,j-next)))
+(defun next-node (i j dir) (neibour-node i j dir 1))
+(defun last-node (i j dir) (neibour-node i j dir -1))
+
+(defun link-p (i j dir)
+  "节点(i j)向dir方向是否连同"
+  ())
+
+;;;; 
