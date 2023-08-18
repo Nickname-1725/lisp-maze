808c54d14fc875762c86fe66db621e86b4a59052
root <root@JellyFish.myguest.virtualbox.org>
2023-08-18 13:09:23 +0800

    写了关于迷宫算法的代码，但无法检验；迷宫节点中储存的prev、dir-list数据结构的构造尚欠缺，需要改进及函数（宏）的封装；迷宫生成算法使用的是非尾递归形式的递归调用，运行时会消耗栈

--- main.lisp
+++ main.lisp
@@ -11,8 +11,8 @@
 ;    3) dir1, dir2, dir3表示下一节点，即向何方位到达相通的相邻节点
 ;    4) 方位均由'w, 'a, 's, 'd表示
 
-(defparameter *W-maze* 20)
-(defparameter *H-maze* 20)
+(defparameter *W-maze* 5)
+(defparameter *H-maze* 5)
 
 (defun generate-table ()
   "构造一个*H-maze*行、*W-maze*列的迷宫"
@@ -26,11 +26,9 @@
 
 (defparameter *table-maze* (generate-table))
 
-(defun get-node (i-j)
+(defmacro get-node (i-j)
   "根据下标访问元素。根据(i j)坐标访问第i行、第j列节点"
-  (let ((i (nth 0 i-j))
-        (j (nth 1 i-j))) 
-  (nth j (nth i *table-maze*))))
+  `(nth (nth 1 ,i-j) (nth (nth 0 ,i-j) *table-maze*)))
 
 (defun neibour-node (i-j dir step)
   "从节点(i j)出发，向dir方向行走step布"
@@ -53,14 +51,54 @@
         ((eql dir 'd) 'a)))
 
 (defun link-p (i-j dir)
-  "节点(i j)向dir方向是否连同"
+  "节点(i j)向dir方向是否连通（不判断相邻节点的合法性）"
   (let* ((next-i-j (next-node i-j dir))
          (next (get-node next-i-j)))
-    (or (member dir (get-node i-j))
-        (member (opposite-dir dir) next))))
+    (or (member dir (nth 1 (get-node i-j)))
+        (member (opposite-dir dir) (nth 1 next)))))
 
 ;;;; 迷宫的生成算法
-
+(defun i-j-legal-p (i-j)
+  (let ((i (nth 0 i-j))
+        (j (nth 1 i-j)))
+    (and (>= i 0) (< i *H-maze*)
+         (>= j 0) (< j *W-maze*))))
+(defun init-maze (init-i-j)
+  (labels ((todo-list () "创建即将探索的各方位的列表"
+             (let ((todo nil))
+               (loop
+                 (let* ((key (random 4))
+                        (dir (cond ((= key 0) 'w)
+                                   ((= key 1) 'a)
+                                   ((= key 2) 's)
+                                   ((= key 3) 'd))))
+                   (if (member dir todo) nil
+                       (setq todo (cons dir todo)))
+                   "当todo列表填满第4个方位时，返回该列表"
+                   (when (nth 3 todo) (return todo))))))
+           (explore (i-j)
+             (let ((todo (todo-list)))
+               (loop for dir in todo
+                     do (let ((next-i-j (next-node i-j dir)))
+                          "判断相邻节点合法性，且必须尚未连通"
+                          (if (and (i-j-legal-p next-i-j)
+                                   (not (link-p i-j dir)))
+                              (progn
+                                ;给当前节点标记可行方位；给next节点标记prev方位"
+                                (setf (nth 1 (get-node i-j)) (cons dir (nth 1 (get-node i-j))))
+                                (setf (nth 0 (get-node next-i-j)) (opposite-dir dir))
+                                (print next-i-j)
+                                ;继续探索下一节点"
+                                (explore next-i-j))
+                              nil)))
+               ;"在穷尽所有可能的方位后，选择退回上一节点"
+               ;'(do somethin else)
+               )))
+    (if (i-j-legal-p init-i-j)
+        (progn
+          (setf (get-node init-i-j) '(nil ()))
+          (explore init-i-j))
+        nil)))
 
 
 
