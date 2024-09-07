bc7c201b3393f2c340c2c6928d9ab22dd7c3e6d6
Nickname-1725 <nickname-1725@outlook.com>
2024-09-07 19:41:17 +0800

    将上下文环境的准备全都放在game-on函数中了；game-on接收迷宫高度和宽度两个变量，现在可以调整迷宫大小；测试成功

--- main.lisp
+++ main.lisp
@@ -126,87 +126,95 @@
 (defun init-fun ()
   (init-TUI)
   
-  (multiple-value-bind (playground-window timerun-window
-                        message-window input-window tips-window)
-      (init-gameboard 20 20)
-    (dump-text-window timerun-window "time: 00:00.00")
-    (dump-text-window message-window "Press [Space] key to start.")
-    (dump-text-window input-window ">_")
-    (dump-text-window tips-window
-                      (format nil "[W] for up; ~%[A] for left; ~@
-                                   [S] for down; ~%[D] for right; ~@
-                                   [q] to leave the game. ~@
-                                   [r] to reshape the maze and restart the game. "))
-    (ncurses-refresh)
-    
-    (let (user-handler
-          (patch-maze (register-patch-to-window playground-window)))
-      (labels ((game-on ()
-                 (restart-maze 20 20)
-                 (setf user-handler (user-handler-create))
-                 (dump-text-window playground-window (draw-maze))
-                 (dump-text-window message-window "Press [Space] key to start."))
-               (key-input ()
-                 (let* ((code (ncurses-getch))
-                        (ch (if (and (< code 256) (>= code 0))
-                                (code-char code))))
-                   (unless (or (eql #\q ch) (eql #\Q ch) (eql #\esc ch))
-                     (cond ((let ((dir (cond
-                                         ((case ch
-                                            (#\w 'w) (#\a 'a) (#\s 's) (#\d 'd)
-                                            (#\W 'w) (#\A 'a) (#\S 's) (#\D 'd)))
-                                         ((eql +key-up+ code) 'w)
-                                         ((eql +key-left+ code) 'a)
-                                         ((eql +key-down+ code) 's)
-                                         ((eql +key-right+ code) 'd))))
-                              ;(dump-text-window tips-window (format nil "~a." code))
-                              (let ((user-ij (funcall user-handler dir)))
-                                (if user-ij
-                                    (progn
-                                      (funcall patch-maze (car user-ij)
-                                               (1+ (* 2 (cadr user-ij))) "O")
-                                      (if (eq-ij-p user-ij
-                                                   `(,(1- *H-maze*) ,(1- *W-maze*)))
-                                          (re-game-check)
-                                          (key-input))
-                                      t))))) ; 确保结果一定为真，短路其它分支
-                           ((case ch (#\r t) (#\R t))
-                            (re-shape))
-                           (t (key-input))))))
-               (re-game-check ()
-                 (dump-text-window message-window "You win. Another game? [y/n]")
-                 (let* ((code (ncurses-getch))
-                        (ch (if (and (< code 256) (>= code 0))
-                                (code-char code))))
-                   (unless (or (eql #\n ch) (eql #\N ch))
-                     (cond ((or (eql #\y ch) (eql #\Y ch))
-                            ;; 再次开始
-                            (game-on)
-                            (key-input))
+  (let (playground-window timerun-window message-window input-window tips-window
+        user-handler patch-maze h-maze w-maze)
+    (labels ((game-on (h w)
+               (ncurses-clear)
+               (ncurses-refresh)
+               ;; 准备上下文环境
+               (when (or playground-window timerun-window message-window
+                         input-window tips-window)
+                 (ncurses-delwin playground-window)
+                 (ncurses-delwin timerun-window)
+                 (ncurses-delwin message-window)
+                 (ncurses-delwin input-window)
+                 (ncurses-delwin tips-window))
+               (multiple-value-bind (ply-win tim-win msg-win in-win tps-win)
+                   (init-gameboard h w)
+                 (setf playground-window ply-win) (setf timerun-window tim-win)
+                 (setf message-window msg-win) (setf input-window in-win)
+                 (setf tips-window tps-win))
+               (setf patch-maze (register-patch-to-window playground-window))
+               (setf user-handler (user-handler-create))
+               (setf h-maze h) (setf w-maze w)
+               ;; 初始化界面信息
+               (dump-text-window timerun-window "time: 00:00.00")
+               (dump-text-window message-window "Press [Space] key to start.")
+               (dump-text-window input-window ">_")
+               (dump-text-window
+                tips-window
+                (format nil "[W] for up; ~%[A] for left; ~@
+                             [S] for down; ~%[D] for right; ~@
+                             [q] to leave the game. ~@
+                             [r] to reshape the maze and restart the game. "))
+               
+               (restart-maze h w)
+               (dump-text-window playground-window (draw-maze))
+               (dump-text-window message-window "Press [Space] key to start."))
+             (key-input ()
+               (let* ((code (ncurses-getch))
+                      (ch (if (and (< code 256) (>= code 0))
+                              (code-char code))))
+                 (unless (or (eql #\q ch) (eql #\Q ch) (eql #\esc ch))
+                   (cond ((let ((dir (cond
+                                       ((case ch
+                                          (#\w 'w) (#\a 'a) (#\s 's) (#\d 'd)
+                                          (#\W 'w) (#\A 'a) (#\S 's) (#\D 'd)))
+                                       ((eql +key-up+ code) 'w)
+                                       ((eql +key-left+ code) 'a)
+                                       ((eql +key-down+ code) 's)
+                                       ((eql +key-right+ code) 'd))))
+                            ;(dump-text-window tips-window (format nil "~a." code))
+                            (let ((user-ij (funcall user-handler dir)))
+                              (if user-ij
+                                  (progn
+                                    (funcall patch-maze (car user-ij)
+                                             (1+ (* 2 (cadr user-ij))) "O")
+                                    (if (eq-ij-p user-ij
+                                                 `(,(1- *H-maze*) ,(1- *W-maze*)))
+                                        (re-game-check)
+                                        (key-input))
+                                    t))))) ; 确保结果一定为真，短路其它分支
+                         ((case ch (#\r t) (#\R t))
+                          (re-shape))
+                         (t (key-input))))))
+             (re-game-check ()
+               (dump-text-window message-window "You win. Another game? [y/n]")
+               (let* ((code (ncurses-getch))
+                      (ch (if (and (< code 256) (>= code 0))
+                              (code-char code))))
+                 (unless (or (eql #\n ch) (eql #\N ch))
+                   (cond ((or (eql #\y ch) (eql #\Y ch))
+                          ;; 再次开始
+                          (game-on h-maze w-maze)
+                          (key-input))
                            (t (re-game-check))))))
-               (re-shape ()
-                 "调整迷宫的大小"
-                 (let ((h (enter-number input-window))
-                       (w (enter-number input-window)))
-                   (cond
-                     ((or (< h 2) (< w 2))
-                      (dump-text-window message-window
-                                        "height and width share be at least 2. ")
-                      (re-shape))
-                     ((or (> h 50) (> w 50))
-                      (dump-text-window message-window
-                                        "height and width share be at most 50. ")
-                      (re-shape))
-                     (t (multiple-value-bind (ply-win tim-win msg-win in-win tps-win)
-                            (init-gameboard h w)
-                          (setf playground-window ply-win)
-                          (setf timerun-window tim-win)
-                          (setf message-window msg-win)
-                          (setf input-window in-win)
-                          (setf tips-window tps-win)
-                          ; 需要调和尺寸修改后的矛盾
-                          ; 需要拓展game-on的语法规则，并且在环境中增加迷宫尺寸
-                          (game-on) (key-input)))))))
-        (game-on)
-        (key-input))))
+             (re-shape ()
+               "调整迷宫的大小"
+               (let ((h (enter-number input-window))
+                     (w (enter-number input-window)))
+                 (cond
+                   ((or (< h 2) (< w 2))
+                    (dump-text-window message-window
+                                      "height and width share be at least 2. ")
+                    (re-shape))
+                   ((or (> h 50) (> w 50))
+                    (dump-text-window message-window
+                                      "height and width share be at most 50. ")
+                    (re-shape))
+                   (t ; 需要调和尺寸修改后的矛盾
+                    ; 需要拓展game-on的语法规则，并且在环境中增加迷宫尺寸
+                    (game-on h w) (key-input))))))
+      (game-on 20 20)
+      (key-input)))
   (ncurses-endwin))
