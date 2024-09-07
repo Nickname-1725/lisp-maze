0e9fb1797ad6e870f5891d7516991a5e43e931f3
Nickname-1725 <nickname-1725@outlook.com>
2024-09-07 20:32:45 +0800

    修改了ccl和clisp中的打包脚本，从而使得ccl可以正确地打包；clisp测试失败，可能是其cffi的支持不够好

--- package-ccl.lisp
+++ package-ccl.lisp
@@ -1,6 +1,6 @@
 
-(load "lisp-dictionary.asd")
-(asdf:load-system :lisp-dictionary)
+(load "lisp-maze.asd")
+(asdf:load-system :lisp-maze)
 (ccl:save-application #p"./build/foo-ccl" :toplevel-function #'init-fun
                       :prepend-kernel t)
 
--- package-clisp.lisp
+++ package-clisp.lisp
@@ -1,6 +1,6 @@
 
-(load "lisp-dictionary.asd")
-(asdf:load-system :lisp-dictionary)
+(load "lisp-maze.asd")
+(asdf:load-system :lisp-maze)
 (ext:saveinitmem #p"./build/foo-clisp" :init-function 
                  (lambda () (init-fun) (ext:quit))
                  :executable t :quiet t :norc t)
